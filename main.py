437f8eb528f8a2f95958fa7cad20057ca5b7ac73
Marcelo Moraes da Silva <133825994+Jstnt@users.noreply.github.com>
2023-11-23 19:24:59 -0300

    Add files via upload
    atualização 1.2

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from PIL import Image
 import mysql.connector
 from mysql.connector import Error
 
-
-
-
+valor = None
+b = 0
 
 class CadastroAlunos(CTkToplevel):
     def __init__(self, *args, **kwargs):
@@ -146,30 +145,26 @@ class CadastroLivros(CTkToplevel):
             con.close()
 
 class EmprestimosLivros(CTkToplevel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)   
-        
-        self.geometry("400x300")
+    def __init__(self):
+        super().__init__()
+        global b
+        global valor   
+        self.geometry("800x600")
         self.title("Empréstimos de Livros")
         largura = self.winfo_screenwidth()
         altura = self.winfo_screenheight()
-        x = (largura - 500) // 2
-        y = (altura - 400) // 2
+        x = (largura - 800) // 2
+        y = (altura - 600) // 2
+        self.geometry(f"800x600+{x}+{y}")
         self.resizable(0,0)
-        self.geometry(f"500x400+{x}+{y}")
-        
         data = date.today()
         ano = data.year
         mes = data.month
         dia = data.day
 
-        FonteN = CTkFont(weight="bold", size=14)
-
         frame = CTkFrame(self,fg_color="transparent")
         FrameE = CTkFrame(frame,fg_color="transparent")
         FrameT = CTkFrame(frame,fg_color="transparent")
-        FrameC = CTkFrame(frame,fg_color="transparent")
-        FrameL = CTkFrame(frame,fg_color="transparent")
         FrameB = CTkFrame(frame,fg_color="transparent")
 
         self.aluno = CTkLabel(FrameE, text="Nome do Aluno")
@@ -184,63 +179,59 @@ class EmprestimosLivros(CTkToplevel):
         self.entry_DataDev.grid(row = 1, column = 1, sticky='w')
         self.entry_DataDev.delete(0, "end")
         self.entry_DataDev.insert(0, "15")
-
-        self.titulo = CTkLabel(FrameT, text="Título do Livro:") 
-        self.titulo.grid(row = 0, column = 0, sticky='w', padx=(0, 10))
-        self.entry_titulo = CTkEntry(FrameT, width=300)
-        self.entry_titulo.grid(row = 1, column = 0, sticky='w', padx=(0, 15))
-       
-        self.autores = CTkLabel(FrameT, text="Autores:")
-        self.autores.grid(row = 2, column = 0, sticky='w')
-        self.entry_autores = CTkEntry(FrameT, width=300)
-        self.entry_autores.grid(row = 3, column = 0, sticky='w')
-    
-        self.isbn = CTkLabel(FrameT, text="ISBN:")
-        self.isbn.grid(row = 0, column = 1, sticky='w')
-        self.entry_isbn = CTkEntry(FrameT)
-        self.entry_isbn.grid(row = 1, column = 1, sticky='w')
-
-        self.quantidade = CTkLabel(FrameT, text="Quantidade:")
-        self.quantidade.grid(row = 2, column = 1, sticky='w')
-        self.entry_quantidade = CTkEntry(FrameT)
-        self.entry_quantidade.grid(row = 3, column = 1, sticky='w')
-
-        self.genero = CTkLabel(FrameC, text="Gênero ou Categoria:")
-        self.genero.grid(row = 0, column = 0, sticky='w', padx=(0, 10))
-        self.entry_genero = CTkEntry(FrameC, width=300)
-        self.entry_genero.grid(row = 1, column = 0, sticky='w', padx=(0, 15))
-
-        self.ano = CTkLabel(FrameC, text="Ano de Publicação:")
-        self.ano.grid(row = 0, column = 1, sticky='w')
-        self.entry_ano = CTkEntry(FrameC)
-        self.entry_ano.grid(row = 1, column = 1, sticky='w')
-
-        self.editora = CTkLabel(FrameL, text="Editora:")
-        self.editora.grid(row = 0, column = 0, sticky='w', padx=(0,152))
-        self.entry_editora = CTkEntry(FrameL, width=300)
-        self.entry_editora.grid(row = 1, column = 0, sticky='w', padx=(0,152))
-        self.labelv = CTkLabel(FrameL, text="")
-        self.labelv.grid(row = 0, column = 1, sticky='w')
-        self.labelv.grid(row = 1, column = 1, sticky='w')
+        colunas = ('id', 'ISBN','Titulo', 'Autor',  'CDD')
+        self.treeT = ttk.Treeview(FrameT, columns=colunas, show='headings')
+        self.treeT.heading('id', text='ID', anchor='w')
+        self.treeT.column('id', width=100)
+        self.treeT.heading('Titulo', text='Titulo', anchor='w')
+        self.treeT.column('Titulo', width=200)
+        self.treeT.heading('Autor', text='Autor', anchor='w')
+        self.treeT.column('Autor', width=160)
+        self.treeT.heading('ISBN', text='ISBN', anchor='w')
+        self.treeT.column('ISBN', width=100)
+        self.treeT.heading('CDD', text='CDD', anchor='w')
+        self.treeT.column('CDD', width=50)
+        scrollbar = ttk.Scrollbar(FrameT, orient="vertical", command=self.treeT.yview)
+        self.treeT.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side="right", fill="y")
+        self.treeT.pack(pady = 10)
+        frame.pack()
         FrameE.pack()
         FrameT.pack()
-        FrameC.pack()
-        FrameL.pack()
-        frame.pack()
-        FrameB.pack(pady= 20)
-        CTkButton(FrameB,text="Pesquisar Livro").grid(row = 0, column = 0, padx = 3)
+        FrameB.pack()
+        CTkButton(FrameB,text="Pesquisar Livro", command=self.InitPesquisarLivros).grid(row = 0, column = 0, padx = 3)
         CTkButton(FrameB,text="Pesquisar Aluno").grid(row = 0, column = 1, padx = 3)
         CTkButton(FrameB,text="Emprestar").grid(row = 0, column = 2, padx = 3)
 
         self.entry_DataDev.bind("<FocusOut>", self.atualizar_data)
 
-        self.DataEmi = CTkLabel(self, text=f"Data de Emissão: {dia}/{mes}/{ano}     ")
-        self.DataEmi.pack(anchor='e')
-        self.DataDevo = CTkLabel(self, text= f"Data de Devolução: {dia}/{mes}/{ano} ")
-        self.DataDevo.pack(anchor='e')
+        self.DataEmi = CTkLabel(self, text=f"Data de Emissão: {dia}/{mes}/{ano}")
+        self.DataEmi.pack(side='bottom', anchor='se')
+        self.DataDevo = CTkLabel(self, text= f"Data de Devolução: {dia}/{mes}/{ano}")
+        self.DataDevo.pack(side='bottom', anchor='se')
         self.atualizar_data()
+        self.PesquisarLivro = None
 
-       
+        self.bind("<FocusIn>", self.pegarvalor)
+
+    def InitPesquisarLivros(self):
+
+        if self.PesquisarLivro is None or not self.PesquisarLivro.winfo_exists():
+            self.PesquisarLivro = PesquisarLivro()
+            self.PesquisarLivro.grab_set()
+        else:
+            self.PesquisarLivro.grab_set()
+        
+
+
+
+    def pegarvalor(self, record):
+        global valor
+        if b == 1:
+            self.treeT.insert("", "end", values=(valor))
+        else:
+            pass
+
     def atualizar_data(self, event=None):
         data = date.today()
         if self.entry_DataDev.get() != "":
@@ -281,34 +272,46 @@ class DisponibilidadeLivros(CTkToplevel):
         self.tree.column('Quantidade', width=100)
         self.tree.heading('CDD', text='CDD', anchor='w')
         self.tree.column('CDD', width=100)
-        
-        self.BttnPesquisa = CTkButton(FrameP, text = "", width = 20, command=self.executar_busca)
+        scrollbar = ttk.Scrollbar(FrameT, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+
+        lupa = CTkImage(light_image=Image.open("img\lupa.png"), size=(20,20))
+
+        self.BttnPesquisa = CTkButton(FrameP, image=lupa, text = "", width = 20, command= self.executar_busca)
         self.BttnPesquisa.grid(row = 0, column = 1)
 
+        scrollbar.pack(side="right", fill="y")
+        self.tree.pack()
+        self.tree.bind('<<TreeviewSelect>>', self.item_selecionado)
+        FrameP.pack()
+        FrameT.pack()
         
         
 
-        def item_selected(self):
-            for selected_item in self.tree.selection():
-                item = self.tree.item(selected_item)
-                record = item['values']
-                # show a message
-                CTkMessagebox(title='Information', message=','.join(record))
-        self.tree.pack()
-        self.tree.bind('<<TreeviewSelect>>', item_selected)
-        FrameP.pack()
-        FrameT.pack()
+    def item_selecionado(self, event=None):
+        for dadoS in self.tree.selection():
+            item = self.tree.item(dadoS)
+            record = item['values']
+            print(record)
+
+    def limpar_tabela(self):
+        for dado in self.tree.get_children():
+            self.tree.delete(dado)
     def executar_busca(self):        
         con = mysql.connector.connect(user='root', database='bibliotecaeteavs', password='', host='localhost')
         cursor = con.cursor()
-        title = self.pesquisa.get()
         
+        title = self.pesquisa.get()
+        self.limpar_tabela()
         try:
-            cursor.execute("SELECT * FROM livro WHERE titulo = %s", (title,))
+            if title == "":
+                cursor.execute("SELECT * FROM livro")
+            else:
+                cursor.execute("SELECT * FROM livro WHERE titulo = %s", (title,))
+            
             resultados = cursor.fetchall()
 
             for resultado in resultados:
-                print(resultado)
                 # Certifique-se de ajustar os índices de acordo com sua consulta SQL
                 self.tree.insert("", "end", values=(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4], resultado[5]))
 
@@ -318,6 +321,91 @@ class DisponibilidadeLivros(CTkToplevel):
         finally:
             cursor.close()
             con.close()
+
+class PesquisarLivro(CTkToplevel):
+    def __init__(self):
+        super().__init__()
+        style = ttk.Style(self)
+        style.configure("Treeview.Heading", font=('Arial', 10, 'bold'))
+        self.geometry("800x600")
+        self.title("Cadastrar Alunos")
+        largura = self.winfo_screenwidth()
+        altura = self.winfo_screenheight()
+        x = (largura - 800) // 2
+        y = (altura - 600) // 2
+        self.geometry(f"800x600+{x}+{y}")
+        FrameP = CTkFrame(self, fg_color="transparent")
+        FrameT = CTkFrame(self, fg_color="transparent")
+        self.pesquisa = CTkEntry(FrameP, width=600, fg_color= "white", text_color= "black")
+        self.pesquisa.grid(row = 0, column = 0, pady = 10)
+        colunas = ('id', 'ISBN','Titulo', 'Autor',  'CDD','Quantidade')
+        self.tree = ttk.Treeview(FrameT, columns=colunas, show='headings')
+        self.tree.heading('id', text='ID', anchor='w')
+        self.tree.column('id', width=100)
+        self.tree.heading('Titulo', text='Titulo', anchor='w')
+        self.tree.column('Titulo', width=200)
+        self.tree.heading('Autor', text='Autor', anchor='w')
+        self.tree.column('Autor', width=160)
+        self.tree.heading('ISBN', text='ISBN', anchor='w')
+        self.tree.column('ISBN', width=100)
+        self.tree.heading('Quantidade', text='Quantidade', anchor='w')
+        self.tree.column('Quantidade', width=100)
+        self.tree.heading('CDD', text='CDD', anchor='w')
+        self.tree.column('CDD', width=50)
+        scrollbar = ttk.Scrollbar(FrameT, orient="vertical", command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+
+        lupa = CTkImage(light_image=Image.open("img\lupa.png"), size=(20,20))
+
+        self.BttnPesquisa = CTkButton(FrameP, image=lupa, text = "", width = 20, command= self.executar_busca)
+        self.BttnPesquisa.grid(row = 0, column = 1)
+
+        scrollbar.pack(side="right", fill="y")
+        self.tree.pack()
+        self.tree.bind("<Double-1>", self.item_selecionado)
+        FrameP.pack()
+        FrameT.pack()
+        
+        
+
+    def item_selecionado(self, event=None):
+        for dadoS in self.tree.selection():
+            global b
+            global valor
+            item = self.tree.item(dadoS)
+            record = item['values']
+            valor=(record[0],record[1],record[2],record[3],record[5])
+            print(record)
+            self.destroy()
+            b = 1
+
+    def limpar_tabela(self):
+        for dado in self.tree.get_children():
+            self.tree.delete(dado)
+    def executar_busca(self):        
+        con = mysql.connector.connect(user='root', database='bibliotecaeteavs', password='', host='localhost')
+        cursor = con.cursor()
+        
+        title = self.pesquisa.get()
+        self.limpar_tabela()
+        try:
+            if title == "":
+                cursor.execute("SELECT * FROM livro")
+            else:
+                cursor.execute("SELECT * FROM livro WHERE titulo = %s", (title,))
+            
+            resultados = cursor.fetchall()
+
+            for resultado in resultados:
+                # Certifique-se de ajustar os índices de acordo com sua consulta SQL
+                self.tree.insert("", "end", values=(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4], resultado[5]))
+
+        except mysql.connector.Error as err:
+            print(f"Erro: {err}")
+
+        finally:
+            cursor.close()
+            con.close()        
         
 
 class Home(CTk):
@@ -423,6 +511,11 @@ class Login(CTk):
 
 def initHome():
     home = Home()
+    caminho_icone = "img\icone.ico"
+    try:
+        home.iconbitmap(caminho_icone)
+    except CTk.TclError:
+        print("Erro: Arquivo do ícone não encontrado.")
     largura = home.winfo_screenwidth()
     altura = home.winfo_screenheight()
     x = (largura - 750) // 2
@@ -432,9 +525,15 @@ def initHome():
     home.resizable(0,0)
     home
     home.mainloop()   
-#Verificar se o arquivo é o principal
+
+#Iniciar o APP
 if __name__ == "__main__":
     app = Login()
+    caminho_icone = "img\icone.ico"
+    try:
+        app.iconbitmap(caminho_icone)
+    except CTk.TclError:
+        print("Erro: Arquivo do ícone não encontrado.")
     largura = app.winfo_screenwidth()
     altura = app.winfo_screenheight()
     x = (largura - 200) // 2
